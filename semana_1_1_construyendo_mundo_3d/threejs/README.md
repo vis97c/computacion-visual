c0416bdf302fd2489fc4a36766450c0b2343d99f
Victor Saa <vis97c@hotmail.com>
2026-02-10 18:37:56 -0500

    render cube with threejs

--- semana_1_1_construyendo_mundo_3d/threejs/README.md
+++ semana_1_1_construyendo_mundo_3d/threejs/README.md
@@ -1,3 +1,18 @@
-# Semana 1 - 1: Construyendo mundo 3D
+# Taller Construyendo Mundo 3D
 
-Ejercicios de la semana 1 - 1 del curso de computación visual
+Victor Saa
+
+10/02/2026
+
+## Descripción
+
+Este proyecto es una aplicación para renderizar un objeto 3D.
+
+## Implementaciónes
+
+Three.js
+React Three Fiber
+
+## IA
+
+IDE y Autocompletado: Antigravity
--- semana_1_1_construyendo_mundo_3d/threejs/src/App.tsx
+++ semana_1_1_construyendo_mundo_3d/threejs/src/App.tsx
@@ -1,12 +1,30 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { OBJLoader } from "three/addons/loaders/OBJLoader.js";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Suspense } from "react";
 
 import "./App.css";
+
+/**
+ * Carga un modelo 3D
+ * @see https://r3f.docs.pmnd.rs/tutorials/loading-models
+ */
+
+function Model() {
+	const obj = useLoader(OBJLoader, "/cube.obj");
+
+	return <primitive object={obj} />;
+}
 
 function App() {
 	return (
 		<div id="canvas-container">
 			<Canvas>
-				<mesh />
+				<Suspense fallback={null}>
+					<Model />
+					<OrbitControls />
+					<Environment preset="sunset" background />
+				</Suspense>
 			</Canvas>
 		</div>
 	);
