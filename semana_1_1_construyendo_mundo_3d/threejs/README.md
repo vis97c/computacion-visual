7e7b4e8b04a84bdd9489fea9d317f96c5ba1ef8e
Victor Saa <vis97c@hotmail.com>
2026-02-10 22:39:22 -0500

    Improved interface & interactivity

--- semana_1_1_construyendo_mundo_3d/threejs/README.md
+++ semana_1_1_construyendo_mundo_3d/threejs/README.md
@@ -16,3 +16,11 @@ React Three Fiber
 ## IA
 
 IDE y Autocompletado: Antigravity
+
+## Imágenes
+
+## Aprendizajes
+
+Aunque ya había trabajado con Three.js con algunos ejercicios básicos tuve que investigar como extraer la geometría de un modelo y la parte del conteo (porque 12 caras?), otros extras fueron la integración con React y la librerías de three fiber. Igual solo seguí la documentación asi que por ahi bien. Ya tenia el scaffolding de otros proyectos y lo incluí porque hace muy comodo trabajar y estructural el código cuando ya esta configurado.
+
+No registre todo lo que busque, pero algunos recursos como [este](https://discourse.threejs.org/t/how-can-we-get-number-of-triangles-vertices-faces-of-glb-model/37064/2) fueron muy utiles
--- semana_1_1_construyendo_mundo_3d/threejs/src/App.tsx
+++ semana_1_1_construyendo_mundo_3d/threejs/src/App.tsx
@@ -1,7 +1,8 @@
+import type { BufferGeometry } from "three";
 import { Canvas, useLoader } from "@react-three/fiber";
 import { OBJLoader } from "three/addons/loaders/OBJLoader.js";
-import { Environment, OrbitControls } from "@react-three/drei";
-import { Suspense } from "react";
+import { Environment, OrbitControls, Edges } from "@react-three/drei";
+import { Suspense, useState } from "react";
 
 import "./App.css";
 
@@ -10,23 +11,64 @@ import "./App.css";
  * @see https://r3f.docs.pmnd.rs/tutorials/loading-models
  */
 
-function Model() {
+function getGeometry() {
+	let vertices = 0;
+	let faces = 0;
+	let geometry: BufferGeometry | undefined = undefined;
+
+	// Esto nos da un grupo con los objetos del modelo
 	const obj = useLoader(OBJLoader, "/cube.obj");
 
-	return <primitive object={obj} />;
+	obj.traverse((child) => {
+		if (child.isObject3D && "geometry" in child) {
+			// Extraer la geometria del modelo
+			geometry = child.geometry as BufferGeometry;
+
+			// Conteo de vertices
+			vertices = geometry?.attributes.position.count || 0;
+
+			// Conteo de caras
+			if (geometry?.index !== null) {
+				faces = (geometry?.index.count || 0) / 3;
+			} else {
+				faces = geometry.attributes.position.count / 3 || 0;
+			}
+		}
+	});
+
+	// Renderizamos un mesh con la geometria extraida
+	// Usamos edges para dibujar las aristas
+	return { geometry, vertices, faces };
 }
 
 function App() {
+	const [showEdges, setShowEdges] = useState(false);
+	const { geometry, vertices, faces } = getGeometry();
+
 	return (
-		<div id="canvas-container">
-			<Canvas>
-				<Suspense fallback={null}>
-					<Model />
-					<OrbitControls />
-					<Environment preset="sunset" background />
-				</Suspense>
-			</Canvas>
-		</div>
+		<main id="container">
+			<section id="canvas-container">
+				<Canvas>
+					<Suspense fallback={null}>
+						<mesh geometry={geometry}>
+							<meshBasicMaterial color="red" />
+							{showEdges && (
+								<Edges linewidth={4} scale={1} threshold={15} color="yellow" />
+							)}
+						</mesh>
+						<OrbitControls />
+						<Environment preset="apartment" background />
+					</Suspense>
+				</Canvas>
+			</section>
+			<ul id="canvas-options">
+				<li>
+					<button onClick={() => setShowEdges(!showEdges)}>Alternar aristas</button>
+				</li>
+				<li>Vertices: {vertices}</li>
+				<li>Caras: {faces}</li>
+			</ul>
+		</main>
 	);
 }
 
